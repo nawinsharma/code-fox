fae691ac7e9f330226d7522c197dd5a482014d23
nawinsharma <nawinsharma60@gmail.com>
2026-04-13 22:50:44 +0530

    add new yt link

--- components/landing/sections/hero-video-section.tsx
+++ components/landing/sections/hero-video-section.tsx
@@ -7,14 +7,14 @@ export function HeroVideoSection() {
         <HeroVideoDialog
           className="block dark:hidden"
           animationStyle="from-center"
-          videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+          videoSrc="https://www.youtube.com/embed/13I9T38YeuA"
           // thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
           thumbnailAlt="Hero Video"
         />
         <HeroVideoDialog
           className="hidden dark:block"
           animationStyle="from-center"
-          videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+          videoSrc="https://www.youtube.com/embed/13I9T38YeuA"
           // thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
           thumbnailAlt="Hero Video"
         />
